3e5ed3dac8a279c50b8b5a6c9e9e916f512f1a33
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-20 20:59:37 +0800

    Update pinterest-api-settings.php

--- pinterest-api-settings.php
+++ pinterest-api-settings.php
@@ -4,6 +4,6 @@ define('PINTEREST_APPLICATION_ID', '5089209881387378078');
 
 define('PINTEREST_APPLICATION_SECRET', '9a159ba14e98e2dc09f0593a32bd8ddd1e3459737fdf5f59b99b790488559d8c');
 
-define('PINTEREST_REDIRECT_URI', 'https://google-fb.herokuapp.com/p-callback.php');
+define('PINTEREST_REDIRECT_URI', 'https://acerloginapi.herokuapp.com/p-callback.php');
 
 ?>
