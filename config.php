c838174edc80c1f84f548100572e2b88fae995a9
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-20 20:54:26 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
 	require_once "Facebook/autoload.php";
 
 	$FB = new \Facebook\Facebook([
-		'app_id'      => '850866198747499',
-    'app_secret'     => '7f50b9082aeaedcad11f15e6551f75d4',
+		'app_id'      => '191008252198022',
+    'app_secret'     => 'ef139f6ed10d749ab1f3462cbefc9ae2',
 		'default_graph_version' => 'v2.10'
 	]);
 
